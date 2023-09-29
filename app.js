3e5d97e943ea83f6819230d1b922971f2d30c120
Owais Khan <oaak78692@gmail.com>
2023-09-29 20:25:18 +0530

    installed method-override and update-route inited

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app =express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path =require("path");
+const methodOverride=require("method-override");
 
 const mongoUrl ="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -19,6 +20,7 @@ async function main(){
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
 
 app.get("/",(req,res)=>{
     res.send("Hi! I am root.");
@@ -27,6 +29,13 @@ app.get("/",(req,res)=>{
 app.get("/listings", async (req,res)=>{
     const allListings= await Listing.find({});
     res.render("./listings/index.ejs",{allListings});
+});
+
+//edit route
+app.get("/listings/:id/edit",async (req,res)=>{
+    let {id}=req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/edit.ejs",{listing});
 });
 
 //new route
@@ -46,6 +55,13 @@ app.post("/listings",async (req,res)=>{
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
+});
+
+//Update Route
+app.put("/listings/:id", async (req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
 });
 
 
