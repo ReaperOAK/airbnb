2621203df34feaa489952497dc57fed626084a26
Owais Khan <oaak78692@gmail.com>
2023-11-16 21:05:32 +0530

    review system and its error handling

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/expressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 const mongoUrl = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -32,8 +33,18 @@ app.get("/",(req,res)=>{
     res.send("Hi! I am root.");
 });
 
-const validateListing = (req,res,nest) => {
+const validateListing = (req,res,next) => {
     let {error} = listingSchema.validate(req.body);
+    if (error){
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else{
+        next();
+    };
+};
+
+const validateReview = (req,res,next) => {
+    let {error} = reviewSchema.validate(req.body);
     if (error){
         let errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(400, errMsg);
@@ -63,7 +74,7 @@ app.get("/listings/new", (req,res)=>{
 //show route
 app.get("/listings/:id", wrapAsync(async (req,res)=> {
     let {id} = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("./listings/show.ejs",{listing});
 }));
 
@@ -86,6 +97,19 @@ app.delete("/listings/:id", wrapAsync(async (req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
+}));
+
+//review
+//post route
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req,res)=>{
+    let {id}=req.params;
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+    listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save();
+    res.redirect(`/listings/${id}`);
+
 }));
 
 // app.get("/testListing",async (req,res)=>{
